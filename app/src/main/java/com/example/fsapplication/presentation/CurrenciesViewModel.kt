cf283b8d3c8758e4ab8860c55ca8c906b69cbcff
tusurovec <tusurovec@gmail.com>
2021-04-10 23:37:35 +0700

    test application

--- app/src/main/java/com/example/fsapplication/presentation/CurrenciesViewModel.kt
+++ app/src/main/java/com/example/fsapplication/presentation/CurrenciesViewModel.kt
@@ -4,21 +4,16 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.fsapplication.App
-import com.example.fsapplication.presentation.state.CurrenciesState
+import com.example.fsapplication.domain.entity.Currency
 
 class CurrenciesViewModel : ViewModel() {
 
     private val repository = App.getInstance().repository
 
-    private val _state = MutableLiveData<CurrenciesState>()
-    val state: LiveData<CurrenciesState> = _state
+    private val _currencies = MutableLiveData<List<Currency>>()
+    val currencies: LiveData<List<Currency>> = _currencies
 
-    fun loadCurrencies() {
-        repository?.getCurrencies { _state.value = CurrenciesState.Content(it) }
-        _state.value = CurrenciesState.Loading
-    }
-
-    private fun proceedError(throwable: Throwable) {
-        _state.value = CurrenciesState.Error
+    fun loadCurrencies(){
+        repository?.getCurrencies { _currencies.value = it }
     }
 }
--- app/src/main/java/com/example/fsapplication/ui/MainActivity.kt
+++ app/src/main/java/com/example/fsapplication/ui/MainActivity.kt
@@ -1,16 +1,13 @@
 package com.example.fsapplication.ui
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.fsapplication.R
 import com.example.fsapplication.domain.entity.Currency
 import com.example.fsapplication.presentation.CurrenciesViewModel
-import com.example.fsapplication.presentation.state.CurrenciesState
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -23,38 +20,11 @@ class MainActivity : AppCompatActivity() {
 
 		viewModel = ViewModelProvider(this).get(CurrenciesViewModel::class.java)
 
-		viewModel.state.observe(this, Observer { applyState(it) })
+		viewModel.currencies.observe(this, Observer<List<Currency>> { currencies ->
+			currenciesList.adapter = CurrencyAdapter(currencies)
+			progress.isVisible = false
+		})
 
 		viewModel.loadCurrencies()
-	}
-
-	private fun applyState(state: CurrenciesState) {
-		when (state) {
-			CurrenciesState.Loading    -> applyLoadingState()
-			is CurrenciesState.Content -> applyContentState(state.currencyList)
-			CurrenciesState.Error      -> applyErrorState()
-		}
-	}
-
-	private fun applyLoadingState() {
-		swipeRefresh.isRefreshing = false
-		swipeRefresh.isVisible = false
-		progress.isVisible = true
-	}
-
-	private fun applyContentState(currencies: List<Currency>) {
-		swipeRefresh.isRefreshing = false
-		swipeRefresh.isVisible = true
-		progress.isVisible = false
-
-		currenciesList.adapter = CurrencyAdapter(currencies)
-	}
-
-	private fun applyErrorState() {
-		swipeRefresh.isRefreshing = false
-		swipeRefresh.isVisible = false
-		progress.isVisible = false
-
-		Toast.makeText(this, getString(R.string.error_message), Toast.LENGTH_SHORT).show()
 	}
 }
